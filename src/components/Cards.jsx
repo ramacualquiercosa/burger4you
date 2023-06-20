94d07f9e4183de1d3616f9ffb0054b4c3e65bbb5
dkda2000 <92997969+dkda2000@users.noreply.github.com>
2023-06-20 01:51:55 -0300

    Modifico Cards (productos) para que el carrousel lleve a los anchors sin refrescar página ni perder carrito de compras lleno.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, {useContext, useEffect } from "react";
 import { productsContext } from "../App";
 
 function Test({
+  FiltroProducto,
   allProducts,
   setAllProducts,
   countProducts,
@@ -9,40 +10,81 @@ function Test({
   total,
   setTotal,
 }) {
+
   let products = useContext(productsContext);
+  
+  
+
+  useEffect(() => {
+
+    if (FiltroProducto&&countProducts==0) {
+      console.log (FiltroProducto);
+      const element = document.getElementById(FiltroProducto.toUpperCase());
+      
+      console.log (element);
+      
+      if (element) {
+        // element.scrollIntoView({ behavior: "auto", block: "center" });
+        // scrolledRef.current = true;
+        
+        var headerOffset = 150;
+        var elementPosition = element.getBoundingClientRect().top;
+        var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+      
+        window.scrollTo({
+             top: offsetPosition,
+             behavior: "auto"
+        });
+
+      }     
+    }
+  });
 
   const onAddProduct = (product) => {
     if (allProducts.find((item) => item.id === product.id)) {
       const products = allProducts.map((item) =>
         item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
       );
-      setTotal(total + product.price * product.quantity);
-      setCountProducts(countProducts + product.quantity);
-      return setAllProducts([...products]);
+       setTotal(total + product.price * product.quantity);
+       setCountProducts(countProducts + product.quantity);
+       return setAllProducts([...products]);
     }
 
-    setTotal(total + product.price * product.quantity);
-    setCountProducts(countProducts + product.quantity);
-    setAllProducts([...allProducts, product]);
+     setTotal(total + product.price * product.quantity);
+     setCountProducts(countProducts + product.quantity);
+     setAllProducts([...allProducts, product]);
   };
 
   return (
     <>
       
-      <div className="bg-dark z-0">
-        
+      <div className="bg-dark">
+    
         <div className="mx-auto max-w-1xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+
+          <div className="fixed left-0 right-0 pt-5 top-16 text-xl text-white bg-black">
+          <h2 className="z-1 mb-4 text-3xl tracking-tight  text-center  text-white ">Productos  
+          {FiltroProducto && " / " + FiltroProducto?.toUpperCase()}</h2>
+           
+           </div>
+
+
           <div className="grid grid-cols-1 gap-x-6 gap-y-16 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-16 justify-items-center pt-14 ">
             {products.map((item, index) => (
-              <div>
+              
+            <div >
+              <a id={item.name.split(' ')[0].toUpperCase()}></a>
+              
                 <div
                   key={index}
                   className="aspect-h-1 aspect-w-1 h-96 w-96 object-center overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7"
                 >
-                  <div
+                    <div
                     className="bg-contain bg-no-repeat bg-center h-full"
                     style={{ backgroundImage: `url(${item.img})` }}
-                  ></div>
+                    
+                  >
+                  </div>
                 </div>
                 <div className="h-1/3">
                   <h2 className="mt-4 text-2xl text-white">{item.name}</h2>
@@ -54,16 +96,21 @@ function Test({
                   </h3>
 
                   <button
-                    className="mt-6 w-full transform rounded-md bg-gray-800 px-6 py-3 text-sm font-medium tracking-wide text-white transition-colors duration-300 hover:bg-gray-600 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50"
+                    className="mt-6 w-full rounded-md bg-gray-800 px-6 py-3 text-sm font-medium tracking-wide text-white transition-colors duration-300 hover:bg-gray-600 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50"
                     onClick={() => onAddProduct(item)}
                   >
                     Agregar a mi carrito
                   </button>
                 </div>
               </div>
-            ))}
+            ))
+            
+            }
+
           </div>
         </div>
+
+        
       </div>
     </>
   );
