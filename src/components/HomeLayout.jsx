e3c95ad2b439087c641fad77c31a14037b1fa11c
Ramiro <ramiroacosta2213@gmail.com>
2023-06-28 20:24:16 -0300

    build

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -8,7 +8,7 @@ function HomeLayout() {
   return (
     <>
     <div class="h-screen px-16 bg-black flex items-center pt-16 " data-aos="zoom-in">
-	<section class="w-full bg-cover bg-center py-32 rounded-lg" style={{ backgroundImage: `url('https://www.infobae.com/new-resizer/7SRvZWr_kQpTejEGhUeMhWZvtmo=/768x432/filters:format(webp):quality(85)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/POXG5OOLHNFO3CY2E3I3ZFDMDA')` }}>
+	<section class="w-full bg-cover bg-center py-32 rounded-lg shadow-inner" style={{ backgroundImage: `url('https://www.infobae.com/new-resizer/7SRvZWr_kQpTejEGhUeMhWZvtmo=/768x432/filters:format(webp):quality(85)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/POXG5OOLHNFO3CY2E3I3ZFDMDA')` }}>
 		<div class="container mx-auto text-center text-white">
 			<h1 class="text-5xl font-medium mb-6">Bienvenido a Burger4U</h1>
 			<p class="text-xl mb-12 px-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio,
